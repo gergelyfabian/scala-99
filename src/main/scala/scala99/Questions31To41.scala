554db64191c0c9fa07c45b343d2fe71e2d871c48
Gergely Fábián <gergo.fb@gmail.com>
2013-10-30 21:06:19 +0100

    Problems 37-41

--- src/main/scala/scala99/Questions31To41.scala
+++ src/main/scala/scala99/Questions31To41.scala
@@ -4,6 +4,16 @@ import scala.annotation.tailrec
 
 object Questions31To41 {
   /**
+   * Prime generator stream
+   */
+  val primes: Stream[Int] = {
+    def primesInt(n: Int): Stream[Int] = {
+      n #:: (primesInt(n + 1).filterNot { x => x % n == 0 })
+    }
+    1 #:: primesInt(2)
+  }
+
+  /**
    * Problem 31
    *
    * Determine whether a given integer number is prime.
@@ -109,4 +119,159 @@ object Questions31To41 {
     }
     primeFactorMultiplicityInt(n, List()).reverse
   }
+
+  /**
+   * Problem 37
+   *
+   * Calculate Euler's totient function phi(m) (improved).
+   *
+   * See problem 34 for the definition of Euler's totient function. If the list of
+   * the prime factors of a number m is known in the form of problem 36 then the
+   * function phi(m) can be efficiently calculated as follows:
+   *
+   * Let ((p1, m1), (p2, m2), (p3, m3), ...) be the list of prime factors (and their
+   * multiplicities) of a given number m.
+   * Then phi(m) can be calculated with the following formula:
+   *
+   * phi(m) = (p1 - 1) * p1 ** (m1 - 1) *
+   *          (p2 - 1) * p2 ** (m2 - 1) *
+   *          (p3 - 1) * p3 ** (m3 - 1) * ...
+   *
+   * Note that a ** b stands for the b'th power of a.
+   *
+   * Example:
+   *   phiImproved(10) === 4
+   */
+  def phiImproved(m: Int): Int = {
+    val factors = primeFactorMultiplicity(m)
+    val totient = factors.foldLeft(1) {
+      case (z, (p, m)) =>
+        z * (p - 1) * math.pow(p, m - 1).toInt
+    }
+    totient
+  }
+
+  /**
+   * Problem 38
+   *
+   * Compare the two methods of calculating Euler's totient function.
+   * Use the solutions of problems P34 and P37 to compare the algorithms.
+   * Try to calculate phi(10090) as an example.
+   *
+   * See unit tests.
+   */
+
+  /**
+   * Problem 39
+   *
+   * A list of prime numbers.
+   *
+   * Given a range of integers by its lower and upper limit, construct a list of all prime numbers in that range.
+   *
+   * Example:
+   *   listPrimesInRange(10, 31) === List(11, 13, 17, 19, 23, 29, 31)
+   */
+  def listPrimesInRange(n: Int, m: Int): List[Int] = {
+    primes.dropWhile(x => x < n).takeWhile(x => x <= m).toList
+  }
+
+  /**
+   * Problem 40
+   *
+   * Goldbach's conjecture.
+   *
+   * Goldbach's conjecture says that every positive even number greater than 2
+   * is the sum of two prime numbers. E.g. 28 = 5 + 23. It is one of the most
+   * famous facts in number theory that has not been proved to be correct in the
+   * general case. It has been numerically confirmed up to very large numbers
+   * (much larger than Scala's Int can represent).
+   *
+   * Write a function to find the two prime numbers that sum up to a given even integer.
+   *
+   * Example:
+   *   goldbach(28) = 5 + 23
+   */
+  def goldbach(n: Int): (Int, Int) = {
+    if (n % 2 != 0) throw new IllegalArgumentException("Goldbach's conjecture is valid for even numbers only.")
+    else {
+      // Take all primes from the prime stream and for each check whether there is
+      // another prime that sums up to n.
+      // Take care to limit primes to less than or equal to (n - x), to remove infinite
+      // checking of impossible combinations (we'd check for 1 + (1 | 2 | 3 | 5 | ...) endlessly).
+      val primeFound = primes.takeWhile(_ < n).find { x =>
+        primes.takeWhile(_ <= n - x).exists(y => x + y == n)
+      }
+      primeFound match {
+        case None => throw new IllegalArgumentException("You just submitted a number that doesn't fill Goldbach's conjecture: " + n)
+        case Some(num) => (num, n - num)
+      }
+    }
+  }
+
+  /**
+   * Limited Goldbach's conjecture calculation
+   *
+   * Primes used need to be higher than or equal to a limit.
+   *
+   * Example:
+   *   goldbachLimited(200, 50) === Some((61, 139))
+   */
+  def goldbachLimited(n: Int, limit: Int): Option[(Int, Int)] = {
+    if (n % 2 != 0) throw new IllegalArgumentException("Goldbach's conjecture is valid for even numbers only.")
+    else {
+      val limitedPrimes = primes.dropWhile(x => x < limit)
+      // Take all primes from the prime stream and for each check whether there is
+      // another prime that sums up to n.
+      // Take care to limit primes to less than or equal to (n - x), to remove infinite
+      // checking of impossible combinations (we'd check for 1 + (1 | 2 | 3 | 5 | ...) endlessly).
+      val primeFound = limitedPrimes.takeWhile(_ < n).find { x =>
+        limitedPrimes.takeWhile(_ <= n - x).exists(y => x + y == n)
+      }
+      primeFound map { num =>
+        (num, n - num)
+      }
+    }
+  }
+
+  /**
+   * Problem 41
+   *
+   * A list of Goldbach compositions.
+   *
+   * Given a range of integers by its lower and upper limit, return a
+   * list of all even numbers and their Goldbach composition.
+   *
+   * Example:
+   *   goldbachList(10, 16) === List((10,3,7), (12,1,11), (14,1,13), (16,3,13))
+   */
+  def goldbachList(n: Int, m: Int): List[(Int, Int, Int)] = {
+    if (n % 2 != 0 || m % 2 != 0) throw new IllegalArgumentException("Goldbach's conjecture is valid for even numbers only.")
+    else
+      (n to m by 2).map { x =>
+        val gldbch = goldbach(x)
+        (x, gldbch._1, gldbch._2)
+      }.toList
+  }
+
+  /**
+   * Problem 41b
+   *
+   * A list of Goldbach compositions (limited)
+   *
+   * In most cases, if an even number is written as the sum of two prime numbers,
+   * one of them is very small.
+   * Very rarely, the primes are both bigger than, say, 50.
+   * Try to find out how many such cases there are in the range 2..3000.
+   *
+   * Example:
+   *   goldbachListLimited(180, 184, 50) === List((180,53,127), (182,73,109), (184,53,131))
+   */
+  def goldbachListLimited(n: Int, m: Int, limit: Int): List[(Int, Int, Int)] = {
+    if (n % 2 != 0 || m % 2 != 0) throw new IllegalArgumentException("Goldbach's conjecture is valid for even numbers only.")
+    else
+      (n to m by 2).flatMap { x =>
+        goldbachLimited(x, limit).map(y => (x, y._1, y._2))
+      }.toList
+  }
+
 }
--- src/test/scala/scala99/Questions31To41Suite.scala
+++ src/test/scala/scala99/Questions31To41Suite.scala
@@ -72,4 +72,58 @@ class Questions31To41Suite extends FunSuite {
   test("problem36: primeFactorMultiplicity(315)") {
     assert(primeFactorMultiplicity(315) === List((3, 2), (5, 1), (7, 1)))
   }
+
+  test("problem37: phiImproved(10)") {
+    assert(phiImproved(10) === 4)
+  }
+
+  test("problem37: phiImproved(36)") {
+    assert(phiImproved(36) === 12)
+  }
+
+  test("problem38: phiImproved(10090) === phi(10090)") {
+    assert(phiImproved(10090) === phi(10090))
+  }
+
+  test("problem39: listPrimesInRange(10, 31)") {
+    assert(listPrimesInRange(10, 31) === List(11, 13, 17, 19, 23, 29, 31))
+  }
+
+  test("problem40: goldbach(28)") {
+    assert(goldbach(28) === (5, 23))
+  }
+
+  test("problem40: goldbach(100)") {
+    assert(goldbach(100) === (3, 97))
+  }
+
+  test("problem41: goldbachList(10, 30)") {
+    assert(goldbachList(10, 30) === List(
+      (10, 3, 7),
+      (12, 1, 11),
+      (14, 1, 13),
+      (16, 3, 13),
+      (18, 1, 17),
+      (20, 1, 19),
+      (22, 3, 19),
+      (24, 1, 23),
+      (26, 3, 23),
+      (28, 5, 23),
+      (30, 1, 29)))
+  }
+
+  test("problem41: goldbachListLimited(180, 200, 50)") {
+    goldbachListLimited(180, 200, 50) === List(
+      (180, 53, 127),
+      (182, 73, 109),
+      (184, 53, 131),
+      (186, 59, 127),
+      (188, 61, 127),
+      (190, 53, 137),
+      (192, 53, 139),
+      (194, 67, 127),
+      (196, 59, 137),
+      (198, 59, 139),
+      (200, 61, 139))
+  }
 }
